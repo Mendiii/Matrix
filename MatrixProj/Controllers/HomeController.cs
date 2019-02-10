4fec22f39fd7d4f29d55c1e4d2a469645e47c8c3
me <meny2800@gmail.com>
2019-02-10 22:36:44 +0200

    i

---
+++ MatrixProj/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using MatrixProj.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MatrixProj.Controllers
+{
+    public class HomeController : Controller
+    {
+        //welcome page
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //searching page
+        public ActionResult SearchingRepositories()
+        {
+            return View();
+        }
+
+        //Bookmark page
+        public ActionResult DisplayBookmarks()
+        {
+            List<ItemModel> BookmarkList = new List<ItemModel>();
+            if (Session["Bookmarks"] != null)
+            {
+                BookmarkList = (List<ItemModel>)Session["Bookmarks"];
+            }
+            return View(BookmarkList);
+        }
+
+
+        //saving result to user session
+        public void SaveBookmarkToSession(ItemModel bookmarkItem)
+        {
+            List<ItemModel> BookmarkList = new List<ItemModel>();
+
+            if (Session["Bookmarks"] != null)
+            {
+                var elements = (List<ItemModel>)Session["Bookmarks"];
+                foreach (var e in elements)
+                {
+                    if (e.id == bookmarkItem.id)
+                    {
+                        return;
+                    }
+                    BookmarkList.Add(e);
+                }
+                BookmarkList.Add(bookmarkItem);
+                Session["Bookmarks"] = BookmarkList;
+            }
+            else
+            {
+                BookmarkList.Add(bookmarkItem);
+                Session["Bookmarks"] = BookmarkList;
+            }
+        }
+
+    }
+}
---
+++ MatrixProj/Models/Model.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MatrixProj.Models
+{
+    //complex model
+    public class ModelHeader
+    {
+        public int total_count { get; set; }
+        public bool incomplete_results { get; set; }
+        public List<ItemModel> items { get; set; }
+    }
+
+    public class ItemModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string full_name { get; set; }
+        public OwnerModel owner { get; set; }
+        public string html_url { get; set; }
+        
+    }
+
+    public class OwnerModel
+    {
+        public string avatar_url { get; set; }
+    }
+}
